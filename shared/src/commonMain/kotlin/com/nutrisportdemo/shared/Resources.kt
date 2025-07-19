ee1d68909044742b9b3668ba533bbfe3fbf9aa20
federicodebenedictis <federico@chainels.com>
2025-07-19 17:48:19 +0200

    Feat: Add icon and image resources
    This commit introduces a collection of new vector drawable resources for icons and images.
    
    **New Icons:**
    - `alert_triangle.xml`
    - `back_arrow.xml`
    - `book.xml`
    - `check.xml`
    - `close.xml`
    - `delete.xml`
    - `dollar.xml`
    - `edit.xml`
    - `grid.xml`
    - `home.xml`
    - `log_in.xml`
    - `log_out.xml`
    - `map_pin.xml`
    - `menu.xml`
    - `minus.xml`
    - `plus.xml`
    - `right_arrow.xml`
    - `search.xml`
    - `unlock.xml`
    - `user.xml`
    - `vertical_menu.xml`
    - `weight.xml`
    
    **New Images:**
    - `cat.xml`
    - `checkmark_image.xml`
    - `google_logo.xml`
    - `paypal_logo.xml`
    - `shopping_cart_image.xml`
    
    A new `Resources.kt` file has been added to provide convenient access to these resources from shared code.

---
+++ shared/src/commonMain/kotlin/com/nutrisportdemo/shared/Resources.kt
@@ -0,0 +1,61 @@
+package com.nutrisportdemo.shared
+
+import nutrisportdemo.shared.generated.resources.Res
+import nutrisportdemo.shared.generated.resources.back_arrow
+import nutrisportdemo.shared.generated.resources.book
+import nutrisportdemo.shared.generated.resources.cat
+import nutrisportdemo.shared.generated.resources.check
+import nutrisportdemo.shared.generated.resources.checkmark_image
+import nutrisportdemo.shared.generated.resources.close
+import nutrisportdemo.shared.generated.resources.delete
+import nutrisportdemo.shared.generated.resources.dollar
+import nutrisportdemo.shared.generated.resources.edit
+import nutrisportdemo.shared.generated.resources.google_logo
+import nutrisportdemo.shared.generated.resources.grid
+import nutrisportdemo.shared.generated.resources.log_in
+import nutrisportdemo.shared.generated.resources.log_out
+import nutrisportdemo.shared.generated.resources.map_pin
+import nutrisportdemo.shared.generated.resources.menu
+import nutrisportdemo.shared.generated.resources.minus
+import nutrisportdemo.shared.generated.resources.paypal_logo
+import nutrisportdemo.shared.generated.resources.plus
+import nutrisportdemo.shared.generated.resources.right_arrow
+import nutrisportdemo.shared.generated.resources.search
+import nutrisportdemo.shared.generated.resources.shopping_cart_image
+import nutrisportdemo.shared.generated.resources.unlock
+import nutrisportdemo.shared.generated.resources.user
+import nutrisportdemo.shared.generated.resources.vertical_menu
+
+/** Share various different resources across the project. */
+object Resources {
+    object Icon {
+        val Plus = Res.drawable.plus
+        val Minus = Res.drawable.minus
+        val SignIn = Res.drawable.log_in
+        val SignOut = Res.drawable.log_out
+        val Unlock = Res.drawable.unlock
+        val Search = Res.drawable.search
+        val Person = Res.drawable.user
+        val Check = Res.drawable.check
+        val Edit = Res.drawable.edit
+        val Menu = Res.drawable.menu
+        val BackArrow = Res.drawable.back_arrow
+        val RightArrow = Res.drawable.right_arrow
+        val ShoppingCart = Res.drawable.shopping_cart_image
+        val Grid = Res.drawable.grid
+        val Dollar = Res.drawable.dollar
+        val MapPin = Res.drawable.map_pin
+        val Close = Res.drawable.close
+        val Book = Res.drawable.book
+        val VerticalMenu = Res.drawable.vertical_menu
+        val Delete = Res.drawable.delete
+    }
+
+    object Image {
+        val ShoppingCart = Res.drawable.shopping_cart_image
+        val Checkmark = Res.drawable.checkmark_image
+        val Cat = Res.drawable.cat
+        val GoogleLogo = Res.drawable.google_logo
+        val PaypalLogo = Res.drawable.paypal_logo
+    }
+}
