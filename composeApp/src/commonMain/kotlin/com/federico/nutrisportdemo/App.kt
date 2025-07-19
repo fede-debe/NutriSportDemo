9258ad4965a00997edadd032950d58d61776e8dd
federicodebenedictis <federico@chainels.com>
2025-07-19 18:23:06 +0200

    Feature: Integrate AuthScreen into App navigation
    This commit introduces navigation to the `AuthScreen` within the `App` composable.
    
    - A new file `NavGraph.kt` is created with a `SetupNavigation` composable.
    - The `SetupNavigation` composable currently displays the `AuthScreen`.
    - The `App` composable now calls `SetupNavigation` to manage the UI.
    - Commented-out code related to `GoogleAuthProvider` initialization and `AnimatedVisibility` has been removed.

--- composeApp/src/commonMain/kotlin/com/federico/nutrisportdemo/App.kt
+++ composeApp/src/commonMain/kotlin/com/federico/nutrisportdemo/App.kt
@@ -2,11 +2,28 @@ package com.federico.nutrisportdemo
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.federico.navigation.SetupNavigation
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+//
+//        var appReady by remember { mutableStateOf(false) }
+//
+//        LaunchedEffect(Unit) {
+//            GoogleAuthProvider.create(credentials = GoogleAuthCredentials(serverId = Constants.WEB_CLIENT_ID))
+//            appReady = true
+//        }
+
+//        AnimatedVisibility(modifier = Modifier.fillMaxSize(), visible = appReady) {
+            SetupNavigation()
+//        }
     }
 }
---
+++ navigation/src/commonMain/kotlin/com/federico/navigation/NavGraph.kt
@@ -0,0 +1,9 @@
+package com.federico.navigation
+
+import androidx.compose.runtime.Composable
+import com.federico.auth.AuthScreen
+
+@Composable
+fun SetupNavigation() {
+    AuthScreen()
+}
