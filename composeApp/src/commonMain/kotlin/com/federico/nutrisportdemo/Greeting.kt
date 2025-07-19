5c452b36bcb96844bf7b3cb9f84c19d416875c31
federicodebenedictis <federico@chainels.com>
2025-07-19 15:01:45 +0200

    Refactor: Remove platform-specific greeting code
    This commit removes the `Greeting` class and associated platform-specific implementations (`Platform.kt`, `Platform.android.kt`, `Platform.ios.kt`).
    
    These files were responsible for generating a platform-specific greeting message, which is no longer needed.

--- composeApp/src/commonMain/kotlin/com/federico/nutrisportdemo/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.federico.nutrisportdemo
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/federico/nutrisportdemo/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.federico.nutrisportdemo
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
