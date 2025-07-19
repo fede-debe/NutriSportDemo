21f79e28537d458bde4f888e0059fed69be7d610
federicodebenedictis <federico@chainels.com>
2025-07-19 18:22:57 +0200

    Feat: Add initial AuthScreen composable
    This commit introduces the `AuthScreen` composable, which serves as the entry point for user authentication.
    
    **Key features:**
    - Displays the application title "NUTRISPORT" and a "Sign in to continue" message.
    - Includes a Google sign-in button.
    - Utilizes `ContentWithMessageBar` for displaying success or error messages.
    - Manages loading state for the Google sign-in button.
    
    **Structure:**
    - The `AuthScreen` is built using a `Scaffold` for basic Material Design layout.
    - The main content is organized within a `Column` that takes up the full screen.
    - The title and sign-in message are centered vertically and horizontally.
    - The Google sign-in button is placed at the bottom of the screen.
    
    **Note:** The actual Google sign-in logic (`GoogleButtonUiContainerFirebase`) is currently commented out.

---
+++ feature/auth/src/commonMain/kotlin/com/federico/auth/AuthScreen.kt
@@ -0,0 +1,97 @@
+package com.federico.auth
+
+import ContentWithMessageBar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.federico.auth.component.GoogleButton
+import com.nutrisportdemo.shared.Alpha
+import com.nutrisportdemo.shared.FontSize
+import com.nutrisportdemo.shared.Surface
+import com.nutrisportdemo.shared.TextPrimary
+import com.nutrisportdemo.shared.TextSecondary
+import com.nutrisportdemo.shared.bebasNeueFont
+import rememberMessageBarState
+
+@Composable
+fun AuthScreen() {
+    val messageBarState = rememberMessageBarState()
+    var loadingState by remember { mutableStateOf(false) }
+
+    Scaffold { paddingValues ->
+        ContentWithMessageBar(
+            contentBackgroundColor = Surface,
+            modifier = Modifier.padding(
+                top = paddingValues.calculateTopPadding(),
+                bottom = paddingValues.calculateBottomPadding()
+            ),
+            messageBarState = messageBarState,
+            errorMaxLines = 2
+        ) {
+            Column(modifier = Modifier.fillMaxSize().padding(all = 24.dp)) {
+                // take full space of the screen weight = 1f
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "NUTRISPORT",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        fontFamily = bebasNeueFont(),
+                        fontSize = FontSize.EXTRA_LARGE,
+                        color = TextSecondary
+                    )
+
+                    Text(
+                        text = "Sign in to continue",
+                        modifier = Modifier.fillMaxWidth().alpha(Alpha.HALF),
+                        textAlign = TextAlign.Center,
+                        fontSize = FontSize.EXTRA_REGULAR,
+                        color = TextPrimary
+                    )
+
+                }
+
+//                GoogleButtonUiContainerFirebase(linkAccount = false, onResult = { result ->
+//                    result.onSuccess { user ->
+//                        messageBarState.addSuccess("Authentication successful!")
+//                        loadingState = false
+//                    }.onFailure { error ->
+//                        if (error.message?.contains("A network error") == true) {
+//                            messageBarState.addError("Internet connection unavailable")
+//                        } else if (error.message?.contains("Idtoken is null") == true) {
+//                            messageBarState.addError("Sign in canceled")
+//                        } else {
+//                            messageBarState.addError(message = error.message ?: "Unknow")
+//                        }
+//                        loadingState = false
+//                    }
+//                }) {
+                    GoogleButton(
+                        loading = loadingState,
+                        onClick = {
+                            loadingState = true
+//                            this@GoogleButtonUiContainerFirebase.onClick()
+                        }
+                    )
+//                }
+            }
+        }
+    }
+}
