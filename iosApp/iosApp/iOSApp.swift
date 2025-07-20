fc2b19a8b6712d2da857fb41e1ba0413a4e09f6f
federicodebenedictis <federico@chainels.com>
2025-07-20 21:52:13 +0200

    feat: Integrate Google Sign-In for iOS
    This commit integrates Google Sign-In functionality into the iOS application.
    
    **Key Changes:**
    
    - **Added GoogleService-Info.plist:** Configures the app with necessary Google service details.
    - **Updated iOSApp.swift:**
        - Added `onOpenURL` modifier to handle Google Sign-In URL schemes.
        - Implemented `AppDelegate` to handle URL opening for Google Sign-In.
    - **Modified project.pbxproj:**
        - Added GoogleSignIn and GoogleSignInSwift frameworks.
        - Included XCRemoteSwiftPackageReference for GoogleSignIn-iOS.
    - **Updated Config.xcconfig:** Removed `TEAM_ID` from `PRODUCT_BUNDLE_IDENTIFIER`.
    - **Added Package.resolved:** Manages Swift Package Manager dependencies, including Google App Check, AppAuth, GoogleSignIn, GoogleUtilities, GTM Session Fetcher, GTMAppAuth, and Promises.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,35 @@
 import SwiftUI
+import GoogleSignIn
 
 @main
 struct iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().onOpenURL(perform: { url in
+                GIDSignIn.sharedInstance.handle(url)
+            })
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+
+
 }
