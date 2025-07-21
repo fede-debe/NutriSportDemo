6ea32263e0940996b61522f7b73eeffca257a812
federicodebenedictis <federico@chainels.com>
2025-07-21 22:55:16 +0200

    Refactor: Introduce basic navigation with Auth screen
    This commit introduces a basic navigation setup using Jetpack Navigation Compose.
    
    - A `Screen` sealed class is defined to represent different navigation destinations.
    - The `SetupNavigation` composable now uses a `NavHost` to manage navigation.
    - The `AuthScreen` is set as the initial destination.

--- navigation/src/commonMain/kotlin/com/federico/navigation/NavGraph.kt
+++ navigation/src/commonMain/kotlin/com/federico/navigation/NavGraph.kt
@@ -1,9 +1,17 @@
 package com.federico.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.federico.auth.AuthScreen
 
 @Composable
 fun SetupNavigation() {
-    AuthScreen()
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Auth) {
+        composable<Screen.Auth> {
+            AuthScreen()
+        }
+    }
 }
---
+++ navigation/src/commonMain/kotlin/com/federico/navigation/Screen.kt
@@ -0,0 +1,9 @@
+package com.federico.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Screen {
+    @Serializable
+    data object Auth: Screen()
+}
