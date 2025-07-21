a7da9038affe5edee9a4a8c56e40dabd69a32e12
federicodebenedictis <federico@chainels.com>
2025-07-21 18:34:38 +0200

    feat: Integrate Firebase SDK and update Google Sign-In
    This commit introduces the Firebase SDK to the iOS project and updates the Google Sign-In configuration.
    
    **Firebase Integration:**
    - Added Firebase SDK (version 12.0.0) as a Swift Package Manager dependency.
    - Configured Firebase in the `AppDelegate` by calling `FirebaseApp.configure()`.
    - Added the following Firebase products to the project:
        - Analytics
        - Auth
        - AuthCombine-Community
        - Core
        - Crashlytics
        - Firestore
        - FirestoreCombine-Community
        - InAppMessaging-Beta
        - Messaging
        - Storage
    
    **Google Sign-In Update:**
    - Updated Google Sign-In related dependencies in `Package.resolved`.
    - Added `GIDServerClientID`, `GIDClientID`, and `CFBundleURLTypes` to `Info.plist` for Google Sign-In configuration.
    - Ensured the `onOpenURL` and `application(_:open:options:)` methods in `iOSApp.swift` and `AppDelegate.swift` respectively handle Google Sign-In URLs.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 import GoogleSignIn
+import Firebase
 
 @main
 struct iOSApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView().onOpenURL(perform: { url in
@@ -14,6 +18,14 @@ struct iOSApp: App {
 
 class AppDelegate: NSObject, UIApplicationDelegate {
 
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
     func application(
       _ app: UIApplication,
       open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
