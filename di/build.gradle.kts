9b1dd3ba274cefb8ead9b3f1cbb461336c36c30b
federicodebenedictis <federico@chainels.com>
2025-07-22 20:53:39 +0200

    Refactor: Introduce Koin modules for dependency injection
    This commit introduces Koin modules to manage dependencies within the application.
    
    A new `sharedModule` is created, which provides a singleton instance of `CustomerRepositoryImpl` (as `CustomerRepository`) and declares `AuthViewModel` as a Koin ViewModel.
    
    The `initializeKoin` function is updated to include the `sharedModule` during Koin initialization.
    
    Dependencies on the `:feature:auth` and `:data` modules are added to `di/build.gradle.kts` to make their components available for injection.

--- di/build.gradle.kts
+++ di/build.gradle.kts
@@ -41,6 +41,9 @@ kotlin {
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
+
+            implementation(project(":feature:auth"))
+            implementation(project(":data"))
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- di/src/commonMain/kotlin/com/federico/di/KoinModule.kt
+++ di/src/commonMain/kotlin/com/federico/di/KoinModule.kt
@@ -1,7 +1,22 @@
 package com.federico.di
 
+import com.federico.auth.AuthViewModel
+import com.federico.data.CustomerRepositoryImpl
+import com.federico.data.domain.CustomerRepository
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+/** @viewModelOf --> So this is one a special syntax in a coin dependency injection library that allows you to specify the
+ * actual type in which you want to inject those other different types.
+ * With this syntax you don't need to actually provide that repository within the ViewModel itself,
+ * we just create the singleton for our repository.
+ */
+val sharedModule = module {
+    single<CustomerRepository> { CustomerRepositoryImpl() }
+    viewModelOf(::AuthViewModel)
+}
 
 /** We will pass the Android context as config parameter*/
 fun initializeKoin(
@@ -9,5 +24,6 @@ fun initializeKoin(
 ) {
     startKoin {
         config?.invoke(this)
+        modules(sharedModule)
     }
 }
