d550dc199e45b8990bd860b66b719c82a8cfda8e
federicodebenedictis <federico@chainels.com>
2025-07-22 09:28:33 +0200

    Refactor: Initialize Koin in Android Application and iOS MainViewController
    This commit moves Koin initialization to the Android `MyApplication` class and the iOS `MainViewController`.
    
    On Android, Koin is now initialized in the `onCreate` method of `MyApplication`, ensuring it's available early in the application lifecycle. The Android application context is also provided to Koin.
    
    On iOS, Koin is initialized within the `configure` block of `ComposeUIViewController` in `MainViewController.kt`. This ensures Koin is set up when the iOS application starts.

--- composeApp/src/androidMain/kotlin/com/federico/nutrisportdemo/MyApplication.kt
+++ composeApp/src/androidMain/kotlin/com/federico/nutrisportdemo/MyApplication.kt
@@ -1,12 +1,19 @@
 package com.federico.nutrisportdemo
 
 import android.app.Application
+import com.federico.di.initializeKoin
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.initialize
+import org.koin.android.ext.koin.androidContext
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initializeKoin(
+            config = {
+                androidContext(this@MyApplication)
+            }
+        )
         Firebase.initialize(context = this)
     }
 }
--- composeApp/src/iosMain/kotlin/com/federico/nutrisportdemo/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/federico/nutrisportdemo/MainViewController.kt
@@ -1,5 +1,10 @@
 package com.federico.nutrisportdemo
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.federico.di.initializeKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+/** configure parameter will be triggered whenever the iOS application starts, and this is why
+ * we are initializing Koin here. */
+fun MainViewController() = ComposeUIViewController(
+    configure = { initializeKoin() }
+) { App() }
