d3646179eed79f3bfe86e133478f093c3f9906a1
federicodebenedictis <federico@chainels.com>
2025-07-23 13:44:46 +0200

    feat: Implement BottomBar in HomeGraphScreen
    This commit introduces the `BottomBar` composable and integrates it into the `HomeGraphScreen`.
    
    **New Composable:**
    - `BottomBar.kt`: A new composable function that displays a bottom navigation bar with icons for different destinations. It includes animation for icon color changes based on selection.
    
    **Changes in `HomeGraphScreen.kt`:**
    - The screen now uses a `Scaffold` to structure its layout.
    - The `BottomBar` is added to the bottom of the screen.
    - The `HomeGraphScreen` is now responsible for handling the selection state and actions of the `BottomBar`.

--- feature/home/src/commonMain/kotlin/com/federico/home/HomeGraphScreen.kt
+++ feature/home/src/commonMain/kotlin/com/federico/home/HomeGraphScreen.kt
@@ -1,8 +1,30 @@
 package com.federico.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.federico.home.component.BottomBar
+import com.nutrisportdemo.shared.Surface
 
 @Composable
 fun HomeGraphScreen() {
+    Scaffold(containerColor = Surface) { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(
+                top = paddingValues.calculateTopPadding(),
+                bottom = paddingValues.calculateBottomPadding()
+            )
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            BottomBar(
+                selected = false,
+                onSelect = { destination ->
 
+                })
+        }
+    }
 }
