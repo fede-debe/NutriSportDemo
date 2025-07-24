da835818c2f6f9e0efa706675175c057ad018f0e
federicodebenedictis <federico@chainels.com>
2025-07-24 09:34:59 +0200

    Implement Custom Drawer with animation
    This commit introduces a custom drawer with animations to the HomeGraphScreen.
    
    **Key changes:**
    - Added `CustomDrawerState` to manage the drawer's open/closed state.
    - Implemented animations for:
        - Drawer offset
        - Home graph scaling
        - Home graph corner radius
        - Background color transition
    - The top app bar navigation icon now animates between a menu icon (drawer closed) and a close icon (drawer open).
    - The `Scaffold` containing the main content is now wrapped in a `Box` to allow for scaling and offset animations when the drawer is open.
    - The `Scaffold` also has a shadow applied when the drawer is open.

--- feature/home/src/commonMain/kotlin/com/federico/home/HomeGraphScreen.kt
+++ feature/home/src/commonMain/kotlin/com/federico/home/HomeGraphScreen.kt
@@ -1,14 +1,19 @@
 package com.federico.home
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -19,8 +24,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -29,6 +40,10 @@ import androidx.navigation.compose.rememberNavController
 import com.federico.home.component.BottomBar
 import com.federico.home.component.CustomDrawer
 import com.federico.home.domain.BottomBarDestination
+import com.federico.home.domain.CustomDrawerState
+import com.federico.home.domain.isOpened
+import com.federico.home.domain.switchState
+import com.nutrisportdemo.shared.Alpha
 import com.nutrisportdemo.shared.FontSize
 import com.nutrisportdemo.shared.IconPrimary
 import com.nutrisportdemo.shared.Resources
@@ -37,6 +52,7 @@ import com.nutrisportdemo.shared.SurfaceLighter
 import com.nutrisportdemo.shared.TextPrimary
 import com.nutrisportdemo.shared.bebasNeueFont
 import com.nutrisportdemo.shared.navigation.Screen
+import com.nutrisportdemo.shared.util.getScreenWidth
 import org.jetbrains.compose.resources.painterResource
 
 /** We can't intercept the system back stack system in the current version of a compose navigation library to update the currentDestination accordingly
@@ -64,77 +80,127 @@ fun HomeGraphScreen() {
         }
     }
 
-    Box(modifier = Modifier.fillMaxSize().background(SurfaceLighter).systemBarsPadding()) {
+    val screenWidth = remember { getScreenWidth() }
+    var drawerState by remember { mutableStateOf(CustomDrawerState.Closed) }
+
+    val offsetValue by remember { derivedStateOf { (screenWidth / 1.5).dp } }
+    /** if drawer is closed we don't want to animate the home graph --> 0.dp */
+    val animatedOffset by animateDpAsState(
+        targetValue = if (drawerState.isOpened()) offsetValue else 0.dp
+    )
+    /** when drawer is opened the size of home graph is 90% of the screen width else full available size(1f) */
+    val animatedScale by animateFloatAsState(
+        targetValue = if (drawerState.isOpened()) 0.9f else 1f
+    )
+
+    val animatedRadius by animateDpAsState(
+        targetValue = if (drawerState.isOpened()) 20.dp else 0.dp
+    )
+
+    val animatedBackground by animateColorAsState(
+        targetValue = if (drawerState.isOpened()) SurfaceLighter else Surface
+    )
+
+    Box(modifier = Modifier.fillMaxSize().background(animatedBackground).systemBarsPadding()) {
         CustomDrawer(
             onProfileClick = {},
             onContactUsClick = {},
             onSignOutClick = {},
             onAdminPanelClick = {}
         )
-        Scaffold(containerColor = Surface, topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    AnimatedContent(targetState = selectedDestination) { destination ->
-                        Text(
-                            destination.title,
-                            fontFamily = bebasNeueFont(),
-                            fontSize = FontSize.LARGE,
-                            color = TextPrimary
-                        )
-                    }
-                },
-                navigationIcon = {
-                    IconButton(onClick = {}) {
-                        Icon(
-                            painter = painterResource(Resources.Icon.Menu),
-                            contentDescription = "Menu icon",
-                            tint = IconPrimary
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Surface,
-                    scrolledContainerColor = Surface,
-                    navigationIconContentColor = IconPrimary,
-                    titleContentColor = TextPrimary,
-                    actionIconContentColor = IconPrimary
-                )
-            )
-        }) { paddingValues ->
-            Column(
-                modifier = Modifier.fillMaxSize().padding(
-                    top = paddingValues.calculateTopPadding(),
-                    bottom = paddingValues.calculateBottomPadding()
-                )
-            ) {
-                NavHost(
-                    modifier = Modifier.weight(1f),
-                    navController = navController,
-                    startDestination = Screen.ProductsOverview
-                ) {
-                    composable<Screen.ProductsOverview> {}
-                    composable<Screen.Cart> {}
-                    composable<Screen.Categories> {}
-                }
-                Spacer(modifier = Modifier.height(12.dp))
-                BottomBar(
-                    selected = selectedDestination,
-                    onSelect = { destination ->
-                        navController.navigate(destination.screen) {
-                            /**
-                             * Pop up to the start destination of the graph when navigate back from a top destination.
-                             * launchSingleTop is being used to not repeat the same screen in the backstack.
-                             *
-                             * When pop from the ProductsOverview screen, inclusive = false make sure it is kept in the backstack.
-                             */
-                            launchSingleTop = true
-                            popUpTo<Screen.ProductsOverview> {
-                                saveState = true
-                                inclusive = false
-                            }
-                            restoreState = true
+        /** we need extra box to provide modifier values to scale down the scaffold */
+        Box(
+            modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(animatedRadius))
+                .offset(x = animatedOffset).scale(scale = animatedScale)
+                .shadow(elevation = 20.dp, shape = RoundedCornerShape(animatedRadius), ambientColor = Color.Black.copy(
+                    Alpha.DISABLED), spotColor = Color.Black.copy(
+                    Alpha.DISABLED))
+        ) {
+            Scaffold(containerColor = Surface, topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        AnimatedContent(targetState = selectedDestination) { destination ->
+                            Text(
+                                destination.title,
+                                fontFamily = bebasNeueFont(),
+                                fontSize = FontSize.LARGE,
+                                color = TextPrimary
+                            )
                         }
-                    })
+                    },
+                    navigationIcon = {
+                        AnimatedContent(targetState = drawerState) { drawer ->
+                            when {
+                                drawer.isOpened() -> {
+                                    IconButton(onClick = {
+                                        drawerState = drawerState.switchState()
+                                    }) {
+                                        Icon(
+                                            painter = painterResource(Resources.Icon.Close),
+                                            contentDescription = "Close icon",
+                                            tint = IconPrimary
+                                        )
+                                    }
+                                }
+
+                                else -> {
+                                    IconButton(onClick = {
+                                        drawerState = drawerState.switchState()
+                                    }) {
+                                        Icon(
+                                            painter = painterResource(Resources.Icon.Menu),
+                                            contentDescription = "Menu icon",
+                                            tint = IconPrimary
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = Surface,
+                        scrolledContainerColor = Surface,
+                        navigationIconContentColor = IconPrimary,
+                        titleContentColor = TextPrimary,
+                        actionIconContentColor = IconPrimary
+                    )
+                )
+            }) { paddingValues ->
+                Column(
+                    modifier = Modifier.fillMaxSize().padding(
+                        top = paddingValues.calculateTopPadding(),
+                        bottom = paddingValues.calculateBottomPadding()
+                    )
+                ) {
+                    NavHost(
+                        modifier = Modifier.weight(1f),
+                        navController = navController,
+                        startDestination = Screen.ProductsOverview
+                    ) {
+                        composable<Screen.ProductsOverview> {}
+                        composable<Screen.Cart> {}
+                        composable<Screen.Categories> {}
+                    }
+                    Spacer(modifier = Modifier.height(12.dp))
+                    BottomBar(
+                        selected = selectedDestination,
+                        onSelect = { destination ->
+                            navController.navigate(destination.screen) {
+                                /**
+                                 * Pop up to the start destination of the graph when navigate back from a top destination.
+                                 * launchSingleTop is being used to not repeat the same screen in the backstack.
+                                 *
+                                 * When pop from the ProductsOverview screen, inclusive = false make sure it is kept in the backstack.
+                                 */
+                                launchSingleTop = true
+                                popUpTo<Screen.ProductsOverview> {
+                                    saveState = true
+                                    inclusive = false
+                                }
+                                restoreState = true
+                            }
+                        })
+                }
             }
         }
     }
