037e86d1c3e4ee0df9c54b1270d43adaffd15dc5
federicodebenedictis <federico@chainels.com>
2025-07-25 19:25:17 +0200

    Refactor: Adjust placeholder alpha and add SurfaceSecondary color
    This commit removes the explicit alpha modification for the placeholder text in `CustomTextField.kt` and introduces a new `SurfaceSecondary` color (Orange) in `Colors.kt`.

--- shared/src/commonMain/kotlin/com/nutrisportdemo/shared/component/CustomTextField.kt
+++ shared/src/commonMain/kotlin/com/nutrisportdemo/shared/component/CustomTextField.kt
@@ -6,13 +6,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.TextSelectionColors
-import androidx.compose.material3.TextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
@@ -73,7 +72,6 @@ fun CustomTextField(
                 {
                     Text(
                         text = placeholder,
-                        modifier = Modifier.alpha(Alpha.DISABLED),
                         fontSize = FontSize.REGULAR
                     )
                 }
