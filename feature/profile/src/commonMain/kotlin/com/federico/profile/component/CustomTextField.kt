0d5f4bc39c194dfe9a255b89c017972476e94d95
federicodebenedictis <federico@chainels.com>
2025-07-25 09:41:37 +0200

    Feat: Add CustomTextField composable
    This commit introduces a new composable function, `CustomTextField`, designed for creating customized text input fields.
    
    **Key Features:**
    - **Customizable Appearance:** Allows customization of border color based on error state, container colors, and indicator colors.
    - **Placeholder Text:** Supports optional placeholder text with configurable alpha.
    - **Enabled/Disabled State:** Can be easily enabled or disabled.
    - **Error State:** Visually indicates errors with a distinct border color.
    - **Expandable:** Supports single-line and multi-line input.
    - **Keyboard Options:** Allows specification of keyboard type.
    - **Theming:** Uses predefined color values from `com.nutrisportdemo.shared` for consistency.

---
+++ feature/profile/src/commonMain/kotlin/com/federico/profile/component/CustomTextField.kt
@@ -0,0 +1,77 @@
+package com.federico.profile.component
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.nutrisportdemo.shared.Alpha
+import com.nutrisportdemo.shared.BorderError
+import com.nutrisportdemo.shared.BorderIdle
+import com.nutrisportdemo.shared.FontSize
+import com.nutrisportdemo.shared.SurfaceLighter
+import com.nutrisportdemo.shared.TextPrimary
+
+@Composable
+fun CustomTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String? = null,
+    enabled: Boolean = true,
+    isError: Boolean = false,
+    expanded: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+) {
+    val borderColor by animateColorAsState(
+        targetValue = if (isError) BorderError else BorderIdle
+    )
+
+    val textFieldColors = TextFieldDefaults.colors(
+        focusedTextColor = TextPrimary,
+        unfocusedTextColor = TextPrimary.copy(alpha = Alpha.DISABLED),
+        disabledTextColor = TextPrimary.copy(alpha = Alpha.DISABLED),
+
+        focusedContainerColor = SurfaceLighter,
+        unfocusedContainerColor = SurfaceLighter,
+        disabledContainerColor = SurfaceLighter,
+        errorContainerColor = SurfaceLighter,
+
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        disabledIndicatorColor = Color.Transparent,
+        errorIndicatorColor = Color.Transparent
+    )
+
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier.fillMaxWidth()
+            .border(width = 1.dp, color = borderColor, shape = RoundedCornerShape(6.dp)),
+        enabled = enabled,
+        placeholder =
+            if (placeholder != null) {
+                {
+                    Text(
+                        text = placeholder,
+                        modifier = Modifier.alpha(Alpha.DISABLED),
+                        fontSize = FontSize.REGULAR
+                    )
+                }
+            } else null,
+        singleLine = !expanded,
+        shape = RoundedCornerShape(size = 6.dp),
+        keyboardOptions = keyboardOptions,
+        colors = textFieldColors
+    )
+}
