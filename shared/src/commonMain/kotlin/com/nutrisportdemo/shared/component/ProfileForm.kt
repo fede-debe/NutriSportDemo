60251a4ba8f3613b761642e6851f57370d209205
federicodebenedictis <federico@chainels.com>
2025-07-25 16:12:57 +0200

    Feat: Add ProfileForm composable
    This commit introduces a new composable function, `ProfileForm`, designed for displaying and editing user profile information.
    
    The `ProfileForm` includes the following fields:
    - First Name
    - Last Name
    - Email (read-only)
    - City
    - Postal Code
    - Address
    - Phone Number
    
    Each editable field utilizes the `CustomTextField` composable and includes basic validation for string length. The form is scrollable and handles IME padding.

---
+++ shared/src/commonMain/kotlin/com/nutrisportdemo/shared/component/ProfileForm.kt
@@ -0,0 +1,93 @@
+package com.nutrisportdemo.shared.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProfileForm(
+    modifier: Modifier = Modifier,
+    firstName: String,
+    onFirstNameChange: (String) -> Unit,
+    lastName: String,
+    onLastNameChange: (String) -> Unit,
+    email: String,
+    city: String,
+    onCityChange: (String) -> Unit,
+    postalCode: Int?,
+    onPostalCodeChange: (Int?) -> Unit,
+    address: String,
+    onAddressChange: (String) -> Unit,
+    phoneNumber: String?,
+    onPhoneNumberChange: (String) -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(
+                horizontal = 24.dp,
+                vertical = 12.dp
+            )
+            .verticalScroll(state = rememberScrollState())
+            .imePadding(),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        CustomTextField(
+            value = firstName,
+            onValueChange = onFirstNameChange,
+            placeholder = "First Name",
+            isError = firstName.length !in 3..50
+        )
+
+        CustomTextField(
+            value = lastName,
+            onValueChange = onLastNameChange,
+            placeholder = "Last Name",
+            isError = lastName.length !in 3..50
+        )
+
+        CustomTextField(
+            value = email,
+            onValueChange = {},
+            placeholder = "Email",
+            enabled = false
+        )
+
+        CustomTextField(
+            value = city,
+            onValueChange = onCityChange,
+            placeholder = "City",
+            isError = city.length !in 3..50
+        )
+
+        CustomTextField(
+            value = "${postalCode ?: ""}",
+            onValueChange = { onPostalCodeChange(it.toIntOrNull()) },
+            placeholder = "Postal Code",
+            isError = postalCode.toString().length !in 3..8
+        )
+
+        CustomTextField(
+            value = address,
+            onValueChange = onAddressChange,
+            placeholder = "Address",
+            isError = address.length !in 3..50
+        )
+
+        CustomTextField(
+            value = phoneNumber ?: "",
+            onValueChange = onPhoneNumberChange,
+            placeholder = "Address",
+            isError = phoneNumber?.length !in 5..50
+        )
+
+    }
+
+}
