ba46198a7e618ebec18d8c2cffe51fd169acf6db
federicodebenedictis <federico@chainels.com>
2025-07-27 20:17:43 +0200

    feat: Implement initial ManageProductScreen UI
    This commit introduces the basic UI structure for the `ManageProductScreen`.
    
    Key features include:
    - A `Scaffold` with a `TopAppBar` that dynamically displays "New Product" or "Edit Product" based on the presence of a product ID.
    - An image placeholder area with a plus icon, allowing users to add or change a product image.
    - Input fields for product title, description, protein (using `AlertTextField`), weight, flavors, and price.
    - A primary button at the bottom, labeled "Add new product" or "Update" with a corresponding icon, also dependent on the product ID.
    - Utilizes `ContentWithMessageBar` for displaying messages.
    - Implements basic styling and layout using Compose modifiers.

---
+++ feature/admin_panel/manage_product/src/commonMain/kotlin/com/federico/manage_product/ManageProductScreen.kt
@@ -0,0 +1,184 @@
+package com.federico.manage_product
+
+import ContentWithMessageBar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.nutrisportdemo.shared.BorderIdle
+import com.nutrisportdemo.shared.FontSize
+import com.nutrisportdemo.shared.IconPrimary
+import com.nutrisportdemo.shared.Resources
+import com.nutrisportdemo.shared.Surface
+import com.nutrisportdemo.shared.SurfaceLighter
+import com.nutrisportdemo.shared.TextPrimary
+import com.nutrisportdemo.shared.bebasNeueFont
+import com.nutrisportdemo.shared.component.PrimaryButton
+import com.nutrisportdemo.shared.component.textField.AlertTextField
+import com.nutrisportdemo.shared.component.textField.CustomTextField
+import org.jetbrains.compose.resources.painterResource
+import rememberMessageBarState
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ManageProductScreen(id: String?, navigateBack: () -> Unit) {
+
+    val messageBarState = rememberMessageBarState()
+
+    val topBarTitle = when {
+        id == null -> "New Product"
+        else -> "Edit Product"
+    }
+    val buttonTitle = if (id == null) "Add new product" else "Update"
+    val buttonIcon = if (id == null) Resources.Icon.Plus else Resources.Icon.Check
+
+    Scaffold(containerColor = Surface, topBar = {
+        TopAppBar(
+            title = {
+                Text(
+                    topBarTitle,
+                    fontFamily = bebasNeueFont(),
+                    fontSize = FontSize.LARGE,
+                    color = TextPrimary
+                )
+            },
+            navigationIcon = {
+                IconButton(onClick = navigateBack) {
+                    Icon(
+                        painter = painterResource(Resources.Icon.BackArrow),
+                        contentDescription = "Back Arrow icon",
+                        tint = IconPrimary
+                    )
+                }
+            },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = Surface,
+                scrolledContainerColor = Surface,
+                navigationIconContentColor = IconPrimary,
+                titleContentColor = TextPrimary,
+                actionIconContentColor = IconPrimary
+            )
+        )
+    }
+    ) { paddingValues ->
+        ContentWithMessageBar(
+            modifier = Modifier.padding(
+                top = paddingValues.calculateTopPadding(),
+                bottom = paddingValues.calculateBottomPadding()
+            ),
+            messageBarState = messageBarState,
+            contentBackgroundColor = Surface,
+            errorMaxLines = 2
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize().padding(horizontal = 24.dp)
+                    .padding(bottom = 24.dp, top = 12.dp).imePadding()
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f).verticalScroll(rememberScrollState()),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(300.dp)
+                            .clip(RoundedCornerShape(size = 12.dp))
+                            .border(
+                                width = 1.dp,
+                                color = BorderIdle,
+                                shape = RoundedCornerShape(size = 12.dp)
+                            )
+                            .background(SurfaceLighter)
+                            .clickable { },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(Resources.Icon.Plus),
+                            contentDescription = "Plus icon",
+                            tint = IconPrimary
+                        )
+                    }
+
+
+                    CustomTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = "Title"
+                    )
+
+                    CustomTextField(
+                        modifier = Modifier.height(168.dp),
+                        value = "",
+                        onValueChange = {},
+                        placeholder = "Description",
+                        expanded = true
+                    )
+
+                    AlertTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "Protein",
+                        onClick = {}
+                    )
+
+                    CustomTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = "Weight (Optional)",
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
+
+                    CustomTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = "Flavors (Optional)"
+                    )
+
+                    CustomTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = "Price",
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                }
+                PrimaryButton(
+                    text = buttonTitle,
+                    icon = buttonIcon,
+                    onClick = {})
+            }
+        }
+    }
+}
