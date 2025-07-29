dffc680705b9ed6bdd816c0331aae295097fcd97
federicodebenedictis <federico@chainels.com>
2025-07-29 10:58:22 +0200

    Implement Android photo picker functionality
    This commit implements the Android-specific functionality for the `PhotoPicker` class.
    
    **Key Changes:**
    - Uses `rememberLauncherForActivityResult` with `ActivityResultContracts.PickVisualMedia` to launch the system photo picker.
    - Manages the photo picker state using a `mutableStateOf` variable (`openPhotoPicker`).
    - Launches the photo picker when the `open()` function is called and the `openPhotoPicker` state changes.
    - Calls the `onImageSelected` callback with the selected `File` (or `null` if no image is selected).

--- feature/admin_panel/manage_product/src/androidMain/kotlin/com/federico/manage_product/util/PhotoPicker.kt
+++ feature/admin_panel/manage_product/src/androidMain/kotlin/com/federico/manage_product/util/PhotoPicker.kt
@@ -1,14 +1,46 @@
 package com.federico.manage_product.util
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import dev.gitlive.firebase.storage.File
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class PhotoPicker {
+    private var openPhotoPicker by mutableStateOf(false)
+
+    /** When this function is triggered we can update the openPhotoPicker value.
+     * When we trigger this function we also trigger the photo picker within the
+     * InitializePhotoPicker
+     * */
+    actual fun open() {
+        openPhotoPicker = true
+    }
+
     @Composable
     actual fun InitializePhotoPicker(onImageSelected: (File?) -> Unit) {
-    }
+        val openPhotoPickerState = remember { openPhotoPicker }
+        val pickMedia = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.PickVisualMedia()
+        ) { uri ->
+            uri?.let { onImageSelected(File(it)) } ?: onImageSelected(null)
+            openPhotoPicker = false
+        }
 
-    actual fun open() {
+        LaunchedEffect(openPhotoPickerState) {
+            if (openPhotoPickerState) {
+                pickMedia.launch(
+                    PickVisualMediaRequest(
+                        mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
+                )
+            }
+        }
     }
-
 }
