087565f32a003753de8287c7dc6330c177b2ee03
federicodebenedictis <federico@chainels.com>
2025-07-30 12:56:53 +0200

    Refactor: Improve PhotoPicker state management
    The `openPhotoPicker` variable in the Android implementation of `PhotoPicker` is now explicitly a `MutableState`.
    
    This change clarifies that the `openPhotoPicker` variable is intended to be observed for changes, which is necessary for triggering the photo picker when its value is updated. The logic for opening and closing the picker remains the same.

--- feature/admin_panel/manage_product/src/androidMain/kotlin/com/federico/manage_product/util/PhotoPicker.kt
+++ feature/admin_panel/manage_product/src/androidMain/kotlin/com/federico/manage_product/util/PhotoPicker.kt
@@ -8,29 +8,29 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import dev.gitlive.firebase.storage.File
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class PhotoPicker {
-    private var openPhotoPicker by mutableStateOf(false)
+    /** openPhotoPicker has to be a MutableState in order to be able to update it.*/
+    private var openPhotoPicker = mutableStateOf(false)
 
     /** When this function is triggered we can update the openPhotoPicker value.
      * When we trigger this function we also trigger the photo picker within the
      * InitializePhotoPicker
      * */
     actual fun open() {
-        openPhotoPicker = true
+        openPhotoPicker.value = true
     }
 
     @Composable
     actual fun InitializePhotoPicker(onImageSelected: (File?) -> Unit) {
-        val openPhotoPickerState = remember { openPhotoPicker }
+        val openPhotoPickerState by remember { openPhotoPicker }
         val pickMedia = rememberLauncherForActivityResult(
             contract = ActivityResultContracts.PickVisualMedia()
         ) { uri ->
             uri?.let { onImageSelected(File(it)) } ?: onImageSelected(null)
-            openPhotoPicker = false
+            openPhotoPicker.value = false
         }
 
         LaunchedEffect(openPhotoPickerState) {
