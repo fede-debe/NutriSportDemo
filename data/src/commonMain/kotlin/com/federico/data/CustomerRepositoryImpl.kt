4d92cfb8463e621a72bbbde5696dd8a0f56cae2b
federicodebenedictis <federico@chainels.com>
2025-08-02 22:43:21 +0200

    Feat: Add isAdmin field and role management to Customer
    This commit introduces an `isAdmin` field to the `Customer` data model and updates the `CustomerRepositoryImpl` to manage user roles in Firestore.
    
    Key changes:
    - **New `isAdmin` field:** The `Customer` data class now includes an `isAdmin` boolean field.
    - **Role creation:** When a new customer is created, a `privateData` subcollection is added with a `role` document, setting `isAdmin` to `false` by default.
    - **Role retrieval:** When fetching customer data, the `isAdmin` status is now retrieved from the `privateData/role` document in Firestore and populated in the `Customer` object.

--- data/src/commonMain/kotlin/com/federico/data/CustomerRepositoryImpl.kt
+++ data/src/commonMain/kotlin/com/federico/data/CustomerRepositoryImpl.kt
@@ -45,6 +45,10 @@ class CustomerRepositoryImpl : CustomerRepository {
                     onSuccess()
                 } else {
                     customerCollection.document(user.uid).set(customer)
+                    customerCollection.document(user.uid)
+                        .collection("privateData")
+                        .document("role")
+                        .set(mapOf("isAdmin" to false))
                     onSuccess()
                 }
             } else {
@@ -66,6 +70,12 @@ class CustomerRepositoryImpl : CustomerRepository {
                         .document(userId).snapshots.collectLatest { document ->
                         when {
                             document.exists -> {
+                                val privateDataDocument = database.collection("customer")
+                                    .document(userId)
+                                    .collection("privateData")
+                                    .document("role")
+                                    .get()
+
                                 /** When this customer changes, the flow will update and send the updated version to the UI */
                                 val customer = Customer(
                                     id = document.id,
@@ -76,7 +86,8 @@ class CustomerRepositoryImpl : CustomerRepository {
                                     postalCode = document.get(field = "postalCode"),
                                     address = document.get(field = "address"),
                                     phoneNumber = document.get(field = "phoneNumber"),
-                                    cart = document.get(field = "cart")
+                                    cart = document.get(field = "cart"),
+                                    isAdmin = privateDataDocument.get(field = "isAdmin")
                                 )
                                 send(RequestState.Success(data = customer))
                             }
