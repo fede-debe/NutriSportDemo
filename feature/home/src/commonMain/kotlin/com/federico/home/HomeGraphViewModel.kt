233ca21f8ad47a3087492ccc240b6af795b86bf5
federicodebenedictis <federico@chainels.com>
2025-08-02 22:44:36 +0200

    Feat: Introduce customer data flow in HomeGraphViewModel
    This commit introduces a new feature to the `HomeGraphViewModel` that allows observing customer data.
    
    - A new `customer` property has been added to `HomeGraphViewModel`.
    - This property is a `StateFlow` that emits `RequestState` objects, representing the current state of customer data fetching (Loading, Success, or Error).
    - The customer data is fetched from `customerRepository.readCustomerFlow()`.
    - The `StateFlow` is configured to be shared while subscribed with a 5-second timeout and starts with an initial state of `RequestState.Loading`.
    - isAdmin will be set from the backend to true if necessary.

--- feature/home/src/commonMain/kotlin/com/federico/home/HomeGraphViewModel.kt
+++ feature/home/src/commonMain/kotlin/com/federico/home/HomeGraphViewModel.kt
@@ -3,12 +3,22 @@ package com.federico.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.federico.data.domain.CustomerRepository
+import com.nutrisportdemo.shared.util.RequestState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class HomeGraphViewModel(private val customerRepository: CustomerRepository) : ViewModel() {
+    val customer = customerRepository.readCustomerFlow()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = RequestState.Loading
+        )
+
     fun signOut(onSuccess: () -> Unit, onError: (String) -> Unit) {
         viewModelScope.launch {
             val result = withContext(Dispatchers.IO) {
