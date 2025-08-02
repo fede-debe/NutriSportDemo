ed2aa96a92c5a84890a669b5ca11ed1605b50913
federicodebenedictis <federico@chainels.com>
2025-08-02 20:54:49 +0200

    Feat: Add search functionality to AdminPanelScreen
    This commit introduces a search bar to the `AdminPanelScreen` allowing users to filter products by title.
    
    Key changes:
    - A `SearchBar` is added to the `TopAppBar`. It becomes visible when the search icon is clicked.
    - The `AdminPanelViewModel` now manages a `searchQuery` StateFlow and provides a `filteredProducts` StateFlow that updates based on the search query.
    - The `AdminPanelScreen` now observes `filteredProducts` to display the list of products.
    - An animation is added to the `TopAppBar` to smoothly transition between the title and the search bar.
    - If the search results in no products, an `InfoCard` is displayed indicating that the product was not found.

--- feature/admin_panel/src/commonMain/kotlin/com/federico/admin_panel/AdminPanelScreen.kt
+++ feature/admin_panel/src/commonMain/kotlin/com/federico/admin_panel/AdminPanelScreen.kt
@@ -1,9 +1,12 @@
 package com.federico.admin_panel
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,18 +14,26 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarColors
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.nutrisportdemo.shared.BorderIdle
 import com.nutrisportdemo.shared.ButtonPrimary
 import com.nutrisportdemo.shared.FontSize
 import com.nutrisportdemo.shared.IconPrimary
 import com.nutrisportdemo.shared.Resources
 import com.nutrisportdemo.shared.Surface
+import com.nutrisportdemo.shared.SurfaceLighter
 import com.nutrisportdemo.shared.TextPrimary
 import com.nutrisportdemo.shared.bebasNeueFont
 import com.nutrisportdemo.shared.component.card.InfoCard
@@ -37,45 +48,102 @@ import org.koin.compose.viewmodel.koinViewModel
 fun AdminPanelScreen(navigateToManageProduct: (String?) -> Unit, navigateBack: () -> Unit) {
 
     val viewModel = koinViewModel<AdminPanelViewModel>()
-    val products = viewModel.products.collectAsState()
+    val products = viewModel.filteredProducts.collectAsState()
+    val searchQuery by viewModel.searchQuery.collectAsState()
+    var searchBarVisible by mutableStateOf(false)
 
     Scaffold(
         containerColor = Surface, topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        "Admin panel",
-                        fontFamily = bebasNeueFont(),
-                        fontSize = FontSize.LARGE,
-                        color = TextPrimary
+            AnimatedContent(
+                targetState = searchBarVisible
+            ) { visible ->
+                if (visible) {
+                    /**
+                     * We are not going to use the InputField component to shoe the query result
+                     * this is why expanded is false, and onExpandedChange and onSearch are empty.
+                     * viewModel::updateSearchQuery will trigger the filteredProducts to search
+                     * the product by the title.
+                     * */
+                    SearchBar(
+                        modifier = Modifier.padding(horizontal = 12.dp)
+                            .fillMaxWidth(),
+                        inputField = {
+                            SearchBarDefaults.InputField(
+                                modifier = Modifier.fillMaxWidth(),
+                                query = searchQuery,
+                                onQueryChange = viewModel::updateSearchQuery,
+                                placeholder = {
+                                    Text(
+                                        text = "Search product",
+                                        fontSize = FontSize.REGULAR,
+                                        color = TextPrimary
+                                    )
+                                },
+                                trailingIcon = {
+                                    IconButton(
+                                        modifier = Modifier.size(14.dp),
+                                        onClick = {
+                                            if (searchQuery.isNotEmpty()) viewModel.updateSearchQuery("")
+                                            else searchBarVisible = false
+                                        }) {
+                                        Icon(
+                                            painter = painterResource(Resources.Icon.Close),
+                                            contentDescription = "Close icon",
+                                            tint = IconPrimary
+                                        )
+                                    }
+                                },
+                                expanded = false,
+                                onExpandedChange = {},
+                                onSearch = {})
+                        },
+                        colors = SearchBarColors(
+                            containerColor = SurfaceLighter,
+                            dividerColor = BorderIdle
+                        ),
+                        expanded = false,
+                        onExpandedChange = {},
+                        content = {}
                     )
-                },
-                navigationIcon = {
-                    IconButton(onClick = navigateBack) {
-                        Icon(
-                            painter = painterResource(Resources.Icon.BackArrow),
-                            contentDescription = "Back Arrow icon",
-                            tint = IconPrimary
+
+                } else {
+                    TopAppBar(
+                        title = {
+                            Text(
+                                "Admin panel",
+                                fontFamily = bebasNeueFont(),
+                                fontSize = FontSize.LARGE,
+                                color = TextPrimary
+                            )
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = navigateBack) {
+                                Icon(
+                                    painter = painterResource(Resources.Icon.BackArrow),
+                                    contentDescription = "Back Arrow icon",
+                                    tint = IconPrimary
+                                )
+                            }
+                        },
+                        actions = {
+                            IconButton(onClick = { searchBarVisible = true }) {
+                                Icon(
+                                    painter = painterResource(Resources.Icon.Search),
+                                    contentDescription = "Back Arrow icon",
+                                    tint = IconPrimary
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                            containerColor = Surface,
+                            scrolledContainerColor = Surface,
+                            navigationIconContentColor = IconPrimary,
+                            titleContentColor = TextPrimary,
+                            actionIconContentColor = IconPrimary
                         )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = {}) {
-                        Icon(
-                            painter = painterResource(Resources.Icon.Search),
-                            contentDescription = "Back Arrow icon",
-                            tint = IconPrimary
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Surface,
-                    scrolledContainerColor = Surface,
-                    navigationIconContentColor = IconPrimary,
-                    titleContentColor = TextPrimary,
-                    actionIconContentColor = IconPrimary
-                )
-            )
+                    )
+                }
+            }
         },
         floatingActionButton = {
             FloatingActionButton(
@@ -96,30 +164,43 @@ fun AdminPanelScreen(navigateToManageProduct: (String?) -> Unit, navigateBack: (
                 LoadingCard(modifier = Modifier.fillMaxSize())
             },
             onSuccess = { lastProducts ->
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(
-                            top = padding.calculateTopPadding(),
-                        ),
-                    contentPadding = PaddingValues(
-                        top = 12.dp,
-                        start = 12.dp,
-                        end = 12.dp,
-                        bottom = 48.dp
-                    ),
-                    verticalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    items(
-                        items = lastProducts,
-                        key = { it.id }
-                    ) { product ->
-                        ProductCard(
-                            product = product,
-                            onClick = { navigateToManageProduct(product.id) }
+                AnimatedContent(
+                    targetState = lastProducts
+                ) { products ->
+                    if (products.isNotEmpty()) {
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(
+                                    top = padding.calculateTopPadding(),
+                                ),
+                            contentPadding = PaddingValues(
+                                top = 12.dp,
+                                start = 12.dp,
+                                end = 12.dp,
+                                bottom = 48.dp
+                            ),
+                            verticalArrangement = Arrangement.spacedBy(12.dp)
+                        ) {
+                            items(
+                                items = lastProducts,
+                                key = { it.id }
+                            ) { product ->
+                                ProductCard(
+                                    product = product,
+                                    onClick = { navigateToManageProduct(product.id) }
+                                )
+                            }
+                        }
+                    } else {
+                        InfoCard(
+                            image = Resources.Image.Cat,
+                            title = "Ooops!",
+                            subtitle = "Product not found"
                         )
                     }
                 }
+
             },
             onError = { message ->
                 InfoCard(
