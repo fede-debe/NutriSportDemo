d48f35894e55cf881e85f357326085e209705cac
federicodebenedictis <federico@chainels.com>
2025-08-02 22:43:55 +0200

    Refactor: Pass customer data to CustomDrawer in HomeGraphScreen
    This commit updates `HomeGraphScreen.kt` to collect customer data from the `HomeGraphViewModel` and pass it to the `CustomDrawer` composable.
    
    The `customer` state is collected as a `StateFlow` and provided to the `CustomDrawer`, enabling it to display customer-specific information.

--- feature/home/src/commonMain/kotlin/com/federico/home/HomeGraphScreen.kt
+++ feature/home/src/commonMain/kotlin/com/federico/home/HomeGraphScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -72,10 +73,10 @@ fun HomeGraphScreen(
     navigateToAdminPanel: () -> Unit,
 ) {
     val viewModel = koinViewModel<HomeGraphViewModel>()
+    val customer by viewModel.customer.collectAsState()
     val messageBarState = rememberMessageBarState()
 
     val navController = rememberNavController()
-
     val currentDestination = navController.currentBackStackEntryAsState()
 
     val selectedDestination by remember {
@@ -115,6 +116,7 @@ fun HomeGraphScreen(
 
     Box(modifier = Modifier.fillMaxSize().background(animatedBackground).systemBarsPadding()) {
         CustomDrawer(
+            customer = customer,
             onProfileClick = navigateToProfile,
             onContactUsClick = {},
             onSignOutClick = {
