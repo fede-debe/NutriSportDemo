31491ee16e54da65d054475e930a45e117700dd8
federicodebenedictis <federico@chainels.com>
2025-08-05 10:18:56 +0200

    Feat: Add quantity constants
    This commit introduces two new constants in `Constants.kt`:
    
    - `MAX_QUANTITY`: Defines the maximum allowed quantity for a product (set to 10).
    - `MIN_QUANTITY`: Defines the minimum allowed quantity for a product (set to 1).

--- shared/src/commonMain/kotlin/com/nutrisportdemo/shared/Constants.kt
+++ shared/src/commonMain/kotlin/com/nutrisportdemo/shared/Constants.kt
@@ -2,4 +2,7 @@ package com.nutrisportdemo.shared
 
 object Constants {
     const val WEB_CLIENT_ID = "649626202936-nthfj6gr3bovdki5h3rlt6su4e5svidj.apps.googleusercontent.com"
+
+    const val MAX_QUANTITY = 10
+    const val MIN_QUANTITY = 1
 }
