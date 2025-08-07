6340410dcf05497596be794f49b3052b9bd8dc0c
federicodebenedictis <federico@chainels.com>
2025-08-07 10:58:27 +0200

    Refactor: Remove unnecessary padding modifiers
    This commit removes the `navigationBarsPadding` and `systemBarsPadding` modifiers from `CategorySearchScreen.kt`.
    
    These modifiers were deemed unnecessary for the current layout and have been removed to simplify the codebase.

--- feature/home/categories/category_search/src/commonMain/kotlin/com/federico/category_search/CategorySearchScreen.kt
+++ feature/home/categories/category_search/src/commonMain/kotlin/com/federico/category_search/CategorySearchScreen.kt
@@ -7,10 +7,8 @@ import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
