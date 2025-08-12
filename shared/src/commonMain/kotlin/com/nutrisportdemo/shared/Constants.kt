53427ac91789d197b2d2e5a993586f996a2c0968
federicodebenedictis <federico@chainels.com>
2025-08-12 22:29:04 +0200

    Feat: Add PayPal constants for payment integration
    This commit introduces new constants to `Constants.kt` for PayPal payment integration.
    
    The following constants have been added:
    - `PAYPAL_GET_TOKEN_URL`: URL for the Firebase Cloud Function to retrieve PayPal access tokens.
    - `PAYPAL_CHECKOUT_ENDPOINT`: PayPal API endpoint for creating checkout orders.
    - `RETURN_URL`: URL to redirect the user to after a successful PayPal payment.
    - `CANCEL_URL`: URL to redirect the user to after a canceled PayPal payment.

--- shared/src/commonMain/kotlin/com/nutrisportdemo/shared/Constants.kt
+++ shared/src/commonMain/kotlin/com/nutrisportdemo/shared/Constants.kt
@@ -2,7 +2,12 @@ package com.nutrisportdemo.shared
 
 object Constants {
     const val WEB_CLIENT_ID = "649626202936-nthfj6gr3bovdki5h3rlt6su4e5svidj.apps.googleusercontent.com"
+    const val PAYPAL_GET_TOKEN_URL = "https://us-central1-nutrisportdemo-466412.cloudfunctions.net/paypalGetToken"
 
+    const val PAYPAL_CHECKOUT_ENDPOINT = "https://api-m.sandbox.paypal.com/v2/checkout/orders"
+
+    const val RETURN_URL = "com.federico.nutrisportdemo://paypalpay?success=true"
+    const val CANCEL_URL = "com.federico.nutrisportdemo://paypalpay?cancel=true"
     const val MAX_QUANTITY = 10
     const val MIN_QUANTITY = 1
 }
